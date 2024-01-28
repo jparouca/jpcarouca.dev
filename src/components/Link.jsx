81653852601864a6851ef0bd7a710b066c36bc9a
jparouca <joaopearoucaa@gmail.com>
2024-01-28 04:16:20 -0300

    fixm deploy

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,20 +1,13 @@
 import NextLink from "next/link";
 
-import { isExternalLink } from "@/lib/utils";
-
 export const Link = ({ href = "#", ...rest }) => {
-  const isExternal = isExternalLink(href);
-  if (isExternal) {
-    return (
-      <a
-        href={href}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="link break-words after:content-['_↗']"
-        {...rest}
-      />
-    );
-  }
-
-  return <NextLink href={href} className="link" {...rest} />;
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="link break-words after:content-['_↗']"
+      {...rest}
+    />
+  );
 };
